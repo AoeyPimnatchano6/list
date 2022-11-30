4ed7fd2c2605190c1c42b8f9619919a8a8b09815
AoeyPimnatchano6 <117252953+AoeyPimnatchano6@users.noreply.github.com>
2022-11-30 12:09:33 +0700

    Add files via upload

---
+++ footballteamWhile.py
@@ -0,0 +1,16 @@
+football_team = []
+
+for i in range (11):
+    name = input("Add a player to the football team: ")
+    football_team.append(name)
+print(football_team)
+
+repeat = 'Y'
+while repeat == 'Y':
+    edit = int(input("Which player do you want to change?: "))
+    football_team[edit-1] = input("Enter a new player: ")
+    print(football_team)
+
+    delete = int(input("Which player do you want to delete?: "))
+    del football_team[delete-1]
+    print(football_team)
